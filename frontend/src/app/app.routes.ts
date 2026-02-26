47b497b58af9ac514dff2d01d4709a5326132b4e
ailchenko <velma86@bk.ru>
2026-02-26 16:23:02 +0200

    feat: add main pages into routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent, title: 'Home',},
+  {path: 'Home', redirectTo: '/'},
+  {path: 'About', component: AboutComponent, title: 'About us',},
+  {path: 'Contacts', component: ContactComponent, title: 'Contact us',},
+];
---
+++ frontend/src/app/shared/components/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+  links = ['Home', 'About us', 'Contacts'];
+}
